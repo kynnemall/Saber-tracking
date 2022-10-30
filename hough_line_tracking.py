2765ed1c8db5d067143e9543efe53fc4c4d0a8a4
martin <sideproject1892@gmail.com>
2022-10-30 17:13:16 +0000

    Update settings and processing

--- hough_line_tracking.py
+++ hough_line_tracking.py
@@ -32,29 +32,35 @@ while ret:
     ret, frame = cap.read()
 
     # convert to HSV for simplicity
-    # blur = cv2.GaussianBlur(frame, (5, 5), 0)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, l_b , u_b)
-    kernel = np.ones((3, 3), np.uint8)
-    mask = cv2.dilate(mask, kernel, iterations=1) # dilate mask to make it easier
+    blur = cv2.GaussianBlur(frame, (5, 5), 0)
+    m1 = blur[:, :, 1] > 200
+    m2 = blur[:, :, 2] > 200
+    m3 = blur[:, :, 0] > 240
+    mask = (np.logical_and(m1, m2) + m3).astype(np.uint8)
 
-    low_threshold = 50
-    high_threshold = 150
-    edges = cv2.Canny(mask, low_threshold, high_threshold)
-    # edges = skeletonize(mask > 100).astype(np.uint8)
+    rho = 1                         # distance resolution in pixels of the Hough grid
+    theta = np.pi / 180             # angular resolution in radians of the Hough grid
+    threshold = 50                  # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 50            # minimum number of pixels making up a line
+    max_line_gap = 10               # maximum gap in pixels between connectable line segments
 
     # Run Hough on edge detected image
     # Output "lines" is an array containing endpoints of detected line segments
     lines = cv2.HoughLinesP(
-        edges, rho, theta, threshold, np.array([]),
+        mask, rho, theta, threshold, np.array([]),
         min_line_length, max_line_gap
     )
 
+    gray = np.zeros(frame.shape[:2], np.uint8)
     if isinstance(lines, np.ndarray):
         for line in lines:
             for x1, y1, x2, y2 in line:
-                cv2.line(frame, (x1, y1), (x2, y2), 255, 2)
-    print(f"Found {lines.size} lines")
+                cv2.line(gray, (x1, y1), (x2, y2), 255, 2)
+
+    contours, _ = cv2.findContours(gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for i, contour in enumerate(contours):
+        if cv2.contourArea(contour) < 1000:
+            cv2.drawContours(frame, contours, i, 255, -1)
     resized = resize(frame)
     
     cv2.imshow("frame", resized)
